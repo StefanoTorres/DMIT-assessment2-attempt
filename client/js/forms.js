c4a21149ec7f7d7200422e42897ee86888e62736
StefanoTorres <sptorres@ualberta.com>
2021-02-28 19:48:58 -0700

    nope

--- client/js/forms.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('load',function(e){
-    console.log(e) 
-    url = "http://localhost:5000/api/v1/users"
-    const results = fetch(url)
-    .then(response => response.json())
-    .then(data => data)
-
-    console.log(data)
-})
