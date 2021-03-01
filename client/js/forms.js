bad2eecd267cc9fb24d3c823e11e7dcf32b5165f
StefanoTorres <sptorres@ualberta.com>
2021-02-28 19:41:09 -0700

    Im just trying stuff at this point

--- client/js/forms.js
+++ client/js/forms.js
@@ -1,3 +1,9 @@
+const { response } = require("express")
+
 window.addEventListener('load',function(e){
     console.log(e) 
+    url = "http://localhost:5000/api/v1/users"
+    fetch(url)
+    .then(response => response.json())
+    .then(data => console.log(data))
 })
